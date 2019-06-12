62c1e04ae88e44c7550bdf0ab4fb42150872a8f9
chenhuan <765264087@qq.com>
2019-06-12 15:03:13 +0800

    意见反馈列表

---
+++ application/admin/model/UserFeedback.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: VULCAN
+ * Date: 2019/6/12
+ * Time: 14:07
+ */
+
+namespace app\admin\model;
+
+
+class UserFeedback extends Base
+{
+    protected $updateTime = false;
+
+    public function user()
+    {
+        return $this->belongsTo('User','uid','id');
+    }
+
+    public function getFeedbackData($page,$limit)
+    {
+        $size = ($page-1)*$limit;
+        $data = $this->with(['user'])->limit($size,$limit)->select();
+        $num  = $this->count();
+        $arr = [
+            'code' => 0,
+            'msg'  => '',
+            'count' => $num,
+            'data'  => $data,
+        ];
+        return $arr;
+    }
+}
