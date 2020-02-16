e261f8d02c96c21e26315cd28265689bfe74ca6c
dingliao <1347208998@qq.com>
2020-02-16 14:22:24 +0800

    x

--- application/common/model/SystemMune.php
+++ application/common/model/SystemMune.php
@@ -3,29 +3,26 @@ namespace app\common\model;
 
 use think\Model;
 
-class SystemMune extends Model
-{
-	public function getMuneAll()
-	{
+class SystemMune extends Model {
+	public function getMuneAll() {
 		// $role = session('admin')['role'];
-		$ids = db('system_role')->where('id',1)->find();
-		$data = db('system_mune')->where('id','in',$ids['ids'])->select();
-		return $this->tree($data) ;
+		$ids = db('system_role')->where('id', 1)->find();
+		$data = db('system_mune')->where('id', 'in', $ids['ids'])->select();
+		return $this->tree($data);
 	}
 
-	public function tree($data ,$pid = 0) {
-    	$tmp = array();
-    	foreach ($data as $key => $value) {
-        	if($value['pid'] == $pid) {
-            	$value['tree'] = $this->tree($data ,$value['id']);
-            	$tmp[] = $value;            
-        	}
-   	 	}
-    	return $tmp;
+	public function tree($data, $pid = 0) {
+		$tmp = array();
+		foreach ($data as $key => $value) {
+			if ($value['pid'] == $pid) {
+				$value['tree'] = $this->tree($data, $value['id']);
+				$tmp[] = $value;
+			}
+		}
+		return $tmp;
 	}
 
-	public function getMune()
-	{
+	public function getMune() {
 		$data = $this->all();
 		return $this->tree($data);
 	}
